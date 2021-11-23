70743359fa24d0ec60fb327a86a2e32610b852c6
Olga Lazuk <lazuk81@mail.ru>
2021-11-23 15:55:38 +0300

    refactor(todoitem.jsx):props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export const Header = (props) => {
   return (
     <header className="d-flex gap-3 my-5">
       <input type="text" className="form-control" placeholder=" ... " />
-     
+      <p>{props.todos.text}</p>
       <button className="btn btn-primary">Add</button>
     </header>
   );
--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -1,9 +1,12 @@
 export const Todoitem = (props) => {
   return (
     <div className="d-flex align-items-center gap-5 alert alert-warning">
-      <input type="checkbox" className="form-check-input" />
-
-      <button className="btn-close" />
+      <input
+        type="checkbox"
+        className="form-check-input"
+        value={props.todos.isActive}
+      />
+      <button className="btn-close" value={props.todos.id} />
     </div>
   );
 };
