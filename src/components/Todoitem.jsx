4ddbace8712756755839aa34f2a195a82d168f69
Olga Lazuk <lazuk81@mail.ru>
2021-11-24 17:50:54 +0300

    refactor(todoitem.jsx):HTML

--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -1,18 +1,18 @@
 export const Todoitem = (props) => {
   return (
     <div className="border-elements d-flex gap-3  my-3 alert">
-      <div className="col-10 d-flex align-items-center gap-5 alert ">
+      <div className="col-10 d-flex align-items-center gap-3 alert ">
         <input
           type="checkbox"
-          className="form-check-input "
+          className="form-check-input"
           value={props.keyProps.isActive}
         />
-        <div className="col container-todo-text text-center">
-          <p>{props.keyProps.text}</p>
+        <div className="col container-todo-text form-control text-center">
+          <span>{props.keyProps.text}</span>
         </div>
       </div>
       <div>
-        <button className="btn btn-close " value={props.keyProps.id} />
+        <button className="btn btn-close float-end" value={props.keyProps.id} />
         <input
           type=""
           className="col-12 my-5 input-date text-center"
